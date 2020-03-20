ecc4b915f05d43d9f8e03fc77d4f01880ef48530
Michael Gerver <mgerver@ufl.edu>
2020-03-19 23:06:27 -0400

    Start setTimeout function implementation

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
+// Use Arrow function to print temperatures 
 var tempList = () => {
     document.getElementById('boiling-temp').innerHTML = 212;
     document.getElementById('freezing-temp').innerHTML = 32;
     document.getElementById('water-temp').innerHTML = 73;
 }; 
 
+// Feeds temperature values to tempList function
 tempList([212, 32, 73]);
+
+// setTimeout 
+setTimeout( function() { document.getElementById('good-temp').setAttribute('class','show')
+    }, 3000);
+
+
+// https://www.linkedin.com/learning/learning-functional-programming-with-javascript/simple-callbacks?u=41282748
+// https://www.linkedin.com/learning/learning-ecmascript-6/arrow-functions?u=41282748     
