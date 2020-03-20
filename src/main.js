ac5a14bc11a12e825a2a118d8b66ec7b593df886
Michael Gerver <mgerver@ufl.edu>
2020-03-19 22:04:55 -0400

    Fix transpiling issue and test js code

---
+++ src/main.js
@@ -0,0 +1,10 @@
+// It is always helpful to use comments in your code!
+const boilingTemp = 212;
+/* const freezingTemp = 32;
+const waterTemp = 73;
+
+var addTemp document.getElementById('boiling-temp').innerHTML = boilingTemp;
+document.getElementById('freezing-temp').innerHTML = freezingTemp;
+document.getElementById('water-temp').innerHTML = waterTemp; */
+
+var addTemp = () => {document.getElementById('boiling-temp').innerHTML = boilingTemp};
